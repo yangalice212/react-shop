d311dcadc694ed3a179fcea1029d7d7cac91ba9e
Alice Yang <yangalice212@gmail.com>
2025-03-20 12:04:31 +0800

    chore: configure gh-pages for project deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,9 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import process from 'process';
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === 'production' ? '/react-shop/' : '/',
   plugins: [react()],
-})
+});
